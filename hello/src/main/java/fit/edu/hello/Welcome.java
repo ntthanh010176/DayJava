b30c085d8a13b00807f41a6671f7f75fb86eafe9
ntthanh010176 <ntthanh@hcmct.edu.vn>
2022-12-29 11:42:38 +0700

    lan 2

--- hello/src/main/java/fit/edu/hello/Welcome.java
+++ hello/src/main/java/fit/edu/hello/Welcome.java
@@ -17,6 +17,7 @@ public class Welcome {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Welcome to java");
+        int x;
     }
     
 }
